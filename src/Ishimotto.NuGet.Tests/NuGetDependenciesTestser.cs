f6b1658a14af3766a3ec7984ca7e032474d175b0
TMaster <github321>
2015-02-19 17:04:35 +0200

    Finish PackageManagerDownloader
    Finish writing the new downloader and cleaning the code a bit.
    
    deleting all the files regarding to NuGetDependency and the old way to
    extract dependencies

--- src/Ishimotto.NuGet.Tests/NuGetDependenciesTestser.cs
+++ src/Ishimotto.NuGet.Tests/NuGetDependenciesTestser.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Ishimotto.NuGet.NuGetDependencies;
 using NUnit.Framework;
 
 namespace Ishimotto.NuGet.Tests
@@ -12,82 +11,82 @@ namespace Ishimotto.NuGet.Tests
     [TestFixture]
     public class NuGetDependenciesTestser
     {
-        #region Tests
-        [Test(Description = "Check if the Depnency is built correctly")]
-        public void Check_Dependnecies_Constructions()
-        {
-            //Arrange
-            var id = "TempPackage";
-            var version = "version";
-            string encodedDependency = String.Format("{0}:{1}", id, version);
+        //#region Tests
+        //[Test(Description = "Check if the Depnency is built correctly")]
+        //public void Check_Dependnecies_Constructions()
+        //{
+        //    //Arrange
+        //    var id = "TempPackage";
+        //    var version = "version";
+        //    string encodedDependency = String.Format("{0}:{1}", id, version);
 
-            //Act
+        //    //Act
 
-            var dependency = new NuGetDependency(encodedDependency);
+        //    var dependency = new NuGetDependency(encodedDependency);
 
-            //Assert
+        //    //Assert
 
-            Assert.That(dependency.PackageId, Is.EqualTo(id));
-            Assert.That(dependency.Version, Is.EqualTo(version));
+        //    Assert.That(dependency.PackageId, Is.EqualTo(id));
+        //    Assert.That(dependency.Version, Is.EqualTo(version));
 
-        }
+        //}
 
-        [Test(Description = "Check if the Depdendency constructor can handle null version")]
-        public void Check_Null_Version()
-        {
-            //Arrange
-            var id = "TempPackage";
+        //[Test(Description = "Check if the Depdendency constructor can handle null version")]
+        //public void Check_Null_Version()
+        //{
+        //    //Arrange
+        //    var id = "TempPackage";
 
-            string encodedDependency = String.Format("{0}:", id);
+        //    string encodedDependency = String.Format("{0}:", id);
 
-            //Act
-            var dependency = new NuGetDependency(encodedDependency);
+        //    //Act
+        //    var dependency = new NuGetDependency(encodedDependency);
 
-            //Assert
+        //    //Assert
 
-            Assert.That(dependency.PackageId, Is.EqualTo(id));
-            Assert.That(dependency.Version, Is.EqualTo(string.Empty));
+        //    Assert.That(dependency.PackageId, Is.EqualTo(id));
+        //    Assert.That(dependency.Version, Is.EqualTo(string.Empty));
 
-        }
+        //}
 
-        [Test(Description = "Check if the DepndencyExtractor extract encoded dependencies correctly")]
-        public void Check_Dependency_Extractor()
-        {
-            //Arange
-            var dependencies = GetDependencies(7);
+        //[Test(Description = "Check if the DepndencyExtractor extract encoded dependencies correctly")]
+        //public void Check_Dependency_Extractor()
+        //{
+        //    //Arange
+        //    var dependencies = GetDependencies(7);
 
-            //Act
-            var result = NuGetDependencyExtractor.Extract(String.Join("|", dependencies.Select(depndency => depndency.ToString())));
+        //    //Act
+        //    var result = NuGetDependencyExtractor.Extract(String.Join("|", dependencies.Select(depndency => depndency.ToString())));
 
-            //Assert
-            Assert.That(result, Is.EquivalentTo(dependencies));
-        }
+        //    //Assert
+        //    Assert.That(result, Is.EquivalentTo(dependencies));
+        //}
 
-        [Test(Description = "Check if the DepndencyExtractor extract encoded dependencies with null version correctly")]
-        public void Check_Dependency_Extractor_With_Null_Version()
-        {
+        //[Test(Description = "Check if the DepndencyExtractor extract encoded dependencies with null version correctly")]
+        //public void Check_Dependency_Extractor_With_Null_Version()
+        //{
 
-            //Arange
-            var dependencies = GetDependencies(7);
+        //    //Arange
+        //    var dependencies = GetDependencies(7);
 
-            dependencies.Concat(new[] { new NuGetDependency("package", string.Empty), });
+        //    dependencies.Concat(new[] { new NuGetDependency("package", string.Empty), });
 
-            //Act
-            var result = NuGetDependencyExtractor.Extract(String.Join("|", dependencies.Select(depndency => depndency.ToString())));
+        //    //Act
+        //    var result = NuGetDependencyExtractor.Extract(String.Join("|", dependencies.Select(depndency => depndency.ToString())));
 
-            //Assert
-            Assert.That(result, Is.EquivalentTo(dependencies));
-        } 
-        #endregion
+        //    //Assert
+        //    Assert.That(result, Is.EquivalentTo(dependencies));
+        //} 
+        //#endregion
 
-        /// <summary>
-        /// Build collection of <see cref="NuGetDependency"/>
-        /// </summary>
-        /// <param name="count">How many <see cref="Dependencies"/> should be built</param>
-        /// <returns>new collection of <see cref="NuGetDependency"/></returns>
-        private IEnumerable<NuGetDependency> GetDependencies(int count)
-        {
-            return Enumerable.Repeat(new NuGetDependency("package", "version"), count);
-        }
+        ///// <summary>
+        ///// Build collection of <see cref="NuGetDependency"/>
+        ///// </summary>
+        ///// <param name="count">How many <see cref="Dependencies"/> should be built</param>
+        ///// <returns>new collection of <see cref="NuGetDependency"/></returns>
+        //private IEnumerable<NuGetDependency> GetDependencies(int count)
+        //{
+        //    return Enumerable.Repeat(new NuGetDependency("package", "version"), count);
+        //}
     }
 }
---
+++ src/Ishimotto.NuGet/Dependencies/PackageDto.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using NuGet;
+
+namespace Ishimotto.NuGet.Dependencies
+{
+    /// <summary>
+    /// Represent a reduced version of <see cref="IPackage"/> with the essential properties only
+    /// </summary>
+    public class PackageDto : IEquatable<PackageDto>
+    {
+        #region Properties
+
+        /// <summary>
+        /// The Id of the package
+        /// </summary>
+        public string ID { get; private set; }
+
+        /// <summary>
+        /// The version of the Package
+        /// </summary>
+        public SemanticVersion Version { get; private set; }
+
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Creates new instance of <see cref="PackageDto"/> 
+        /// </summary>
+        /// <param name="package">package to extract info from</param>
+        internal PackageDto(PackageDependency package)
+            : this(package.Id,package.VersionSpec.MaxVersion)
+        {
+            
+        }
+
+        /// <summary>
+        /// Creates new instance of <see cref="PackageDto"/> 
+        /// </summary>
+        /// <param name="package">package to extract info from</param>
+        internal PackageDto(IPackageName package)
+            : this(package.Id, package.Version)
+        {
+
+        }
+
+        /// <summary>
+        /// Creates new instance of <see cref="PackageDto"/> 
+        /// </summary>
+        /// <param name="id">The Id of the package</param>
+        /// <param name="version">The version of the package</param>
+        public PackageDto(string id, string version)
+            : this(id, SemanticVersion.Parse(version))
+        {
+        }
+
+        /// <summary>
+        /// Creates new instance of <see cref="PackageDto"/> 
+        /// </summary>
+        /// <param name="id">The Id of the package</param>
+        /// <param name="version">The version of the package</param>
+        internal PackageDto(string id, SemanticVersion version)
+            
+        {
+            ID = id;
+            Version = version;
+        }
+
+        #endregion
+
+        #region IEquatable Implemntation
+        /// <summary>
+        /// Determines whether 2 <see cref="PackageDto"/> are equals
+        /// </summary>
+        /// <param name="other">Package to compare with the current package</param>
+        /// <returns></returns>
+        public bool Equals(PackageDto other)
+        {
+            return ID == other.ID && Version.Equals(other.Version);
+        }
+
+        #endregion
+    }
+}
